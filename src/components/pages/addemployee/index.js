7a961f8aa840fe1c9951ac83043f31b6e09d0b20
taiqui14 <chautaiqui@gmail.com>
2021-03-25 00:26:11 +0700

    employee, addemployee

---
+++ src/components/pages/addemployee/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+
+import {
+    Form, Input, Button, Radio, Select, DatePicker, InputNumber, Switch, message
+} from 'antd';
+
+export const Addemployee = (props) => {
+    const [ form ] = Form.useForm();
+
+    const onFinish = async values => {
+        console.log(values)
+        try {
+          // post employee 
+        } catch (e) { 
+            message(e);
+        }
+      };
+    
+    const onFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+    };
+    return  (
+        <>
+            <Form
+                form={form}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                labelCol={{
+                    span: 4,
+                }}
+                wrapperCol={{
+                    span: 12,
+                }}
+                layout="horizontal"
+                initialValues={{
+                    email: 'default',
+                }}
+            >
+                <Form.Item label="Email" name="email">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Name" name="name">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Phone" name="contactNumber">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Address" name="address">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Hotel" name="hotel">
+                    <Select>
+                        <Select.Option value="1">Hotel 1</Select.Option>
+                        <Select.Option value="2">Hotel 2</Select.Option>
+                        <Select.Option value="3">Hotel 3</Select.Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item label="DatePicker" name="dateOfBirth">
+                    <DatePicker />
+                </Form.Item>
+                <Form.Item label="Type" name="type">
+                    <Select>
+                        <Select.Option value="1">Manager</Select.Option>
+                        <Select.Option value="2">Employee</Select.Option>
+                        <Select.Option value="3">Intership</Select.Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item label="Department" name="department">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Designation" name="designation">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Type" name="type">
+                    <Select
+                        mode="tags"
+                        placeholder="Choose skill"
+                    >
+                        <Select.Option value="1">English</Select.Option>
+                        <Select.Option value="2">Chinese</Select.Option>
+                        <Select.Option value="3">France</Select.Option>
+                        <Select.Option value="4">Other</Select.Option>
+                    </Select>
+                </Form.Item>
+            </Form>
+            <Button onClick={()=>form.submit()}>Submit</Button>
+        </>
+    )
+}
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 
 import { RocketOutlined, UsergroupAddOutlined, DashboardOutlined,
-  AreaChartOutlined, TeamOutlined, ToolOutlined, HomeOutlined, SelectOutlined
+  AreaChartOutlined, TeamOutlined, ToolOutlined, HomeOutlined, SelectOutlined, GifOutlined
 } from '@ant-design/icons';
+
+import { Employee } from './employee';
+import { Addemployee } from './addemployee';
 const Todo = () => {
   return <h1>To Do</h1>
 }
 
 const subPages1 = [
   { path: '/dashboard', label: (<><DashboardOutlined style={{paddingLeft:10}}/> Dashboard </>), component: Todo, permissions: '' },
-  { path: '/1', label: (<><TeamOutlined style={{paddingLeft:10}}/> Employee </>), component: Todo, permissions: '' },
-  { path: '/2', label: (<><UsergroupAddOutlined style={{paddingLeft:10}}/> Add Employee </>), component: Todo, permissions: '' },
+  { path: '/employee', label: (<><TeamOutlined style={{paddingLeft:10}}/> Employee </>), component: Employee, permissions: '' },
+  { path: '/2', label: (<><UsergroupAddOutlined style={{paddingLeft:10}}/> Add Employee </>), component: Addemployee, permissions: '' },
   { path: '/3', label: (<><ToolOutlined style={{paddingLeft:10}}/> Edit Hotel </>), component: Todo, permissions: '' },
   { path: '/4', label: (<><HomeOutlined style={{paddingLeft:10}}/> Room </>), component: Todo, permissions: '' },
   { path: '/5', label: (<><RocketOutlined style={{paddingLeft:10}} rotate={45}/> Form Resquest </>), component: Todo, permissions: '' },
 ];
 const subPages2 = [
   {path: '/7', label: (<><SelectOutlined /> Booking </>), component: Todo, permissions: ''},
+  {path: '/8', label: (<><GifOutlined /> Voucher </>), component: Todo, permissions: ''},
 ];
 const subPages3 = [
-  {path: '/8', label: (<><AreaChartOutlined /> Inventory Report </>), component: Todo, permissions: ''},
+  {path: '/9', label: (<><AreaChartOutlined /> Inventory Report </>), component: Todo, permissions: ''},
 ];
 export const routerPages = [...subPages1, ...subPages2];
 
